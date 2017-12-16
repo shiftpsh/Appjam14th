93287273cef88045bea834074a8152a50191f419
shiftpsh <shift@v.anil.la>
2017-12-16 21:36:10 +0900

    Added databinding

--- app/src/main/java/kr/laptop/school/petitions/ui/activities/DashboardActivity.java
+++ app/src/main/java/kr/laptop/school/petitions/ui/activities/DashboardActivity.java
@@ -1,13 +1,23 @@
 package kr.laptop.school.petitions.ui.activities;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+
 import kr.laptop.school.petitions.R;
+import kr.laptop.school.petitions.databinding.ActivityDashboardBinding;
 
 class DashboardActivity extends AppCompatActivity {
+
+    private ActivityDashboardBinding binding;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_dashboard);
+        /*
+        ActivityDashboardBinding bindinga = DataBindingUtil.inflate(LayoutInflater.from(this), R.layout.activity_dashboard, null, false);
+        bindinga.getRoot();*/
     }
 }
