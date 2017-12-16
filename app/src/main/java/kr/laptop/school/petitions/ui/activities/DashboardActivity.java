cd038bdae77367128ce67eb29a8f0e2e5a68d7af
shiftpsh <shift@v.anil.la>
2017-12-16 22:35:41 +0900

    Moved build tools version to variable

--- app/src/main/java/kr/laptop/school/petitions/ui/activities/DashboardActivity.java
+++ app/src/main/java/kr/laptop/school/petitions/ui/activities/DashboardActivity.java
@@ -3,20 +3,18 @@ package kr.laptop.school.petitions.ui.activities;
 import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 
 import kr.laptop.school.petitions.R;
 import kr.laptop.school.petitions.databinding.ActivityDashboardBinding;
 
 public class DashboardActivity extends AppCompatActivity {
 
-     ActivityDashboardBinding binding;
+    public ActivityDashboardBinding binding;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_dashboard);
-        /*
-        ActivityDashboardBinding bindinga = DataBindingUtil.inflate(LayoutInflater.from(this), R.layout.activity_dashboard, null, false);
-        bindinga.getRoot();*/
     }
 }
