7edcba0601db9d7e667a42c6f79ad12b1e4276b3
shiftpsh <shift@v.anil.la>
2017-12-16 21:27:27 +0900

    Reverted DashboardActivity to Java

---
+++ app/src/main/java/kr/laptop/school/petitions/ui/activities/DashboardActivity.java
@@ -0,0 +1,13 @@
+package kr.laptop.school.petitions.ui.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import kr.laptop.school.petitions.R;
+
+class DashboardActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
