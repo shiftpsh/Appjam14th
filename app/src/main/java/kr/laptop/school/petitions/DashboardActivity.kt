bb96d2ed73afc3d09b369184c41667c257f34c66
shiftpsh <shift@v.anil.la>
2017-12-16 20:40:03 +0900

    Renamed MainActivity to DashboardActivity

--- app/src/main/java/kr/laptop/school/petitions/DashboardActivity.kt
+++ app/src/main/java/kr/laptop/school/petitions/DashboardActivity.kt
@@ -3,10 +3,9 @@ package kr.laptop.school.petitions
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class DashboardActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
     }
 }
